858bfe332be085c659d4fe97a08db6961123c55b
Lewis Ledesma <lledesma@bu.edu>
2017-12-05 18:11:01 -0500

    Fixed the crash, moved the Compass code to its own java file, still need to work on location

--- app/src/main/java/com/example/lewis/helloworld/MainActivity.java
+++ app/src/main/java/com/example/lewis/helloworld/MainActivity.java
@@ -13,58 +13,16 @@ import android.location.LocationManager;
 import android.content.Context;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorManager.registerListener(this,compassSensor, SensorManager.SENSOR_DELAY_GAME);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
 
 
-    SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    Sensor compassSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-
-    @Override
-        public void onSensorChanged(SensorEvent sensorEvent)
-        {
-                float degree = Math.round(sensorEvent.values[0]);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i)
-        {
-
-        }
-
-    LocationManager locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-
-    LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            location.getLatitude();
-
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-        public void onProviderEnabled(String provider) {
-        }
-
-        public void onProviderDisabled(String provider) {
-        }
-    };
 
 
 }
-
-
