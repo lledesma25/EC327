d509fe8ab28bbbedbad18b4ad78513c75ba3dd1a
Lewis Ledesma <lledesma@bu.edu>
2017-12-09 17:40:15 -0500

    LocationActivity displays the current speed, if not moving, says not moving, if moving displays speed. Added a button that goes to it

--- app/src/main/java/com/example/lewis/helloworld/MainActivity.java
+++ app/src/main/java/com/example/lewis/helloworld/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.lewis.helloworld;
 
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        int permission_track = 1;
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(!hasPermissions(this, permissions))
+        {
+            ActivityCompat.requestPermissions(this, permissions, permission_track);
+        }
 
         if(isServicesOK()){
             init();
@@ -63,4 +77,23 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    public static boolean hasPermissions(Context context, String[] permissions)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null)
+        {
+            for(String permission:permissions)
+            {
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void viewLocationActivity(View view) {
+        Intent viewLoc = new Intent(MainActivity.this, LocationActivity.class);
+        startActivity(viewLoc);
+    }
 }
