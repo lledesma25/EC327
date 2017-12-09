ee579cca87cfa525dfd0b8f4e99bacffd4ce23f3
Ahmed Albahhar <albahhar.ahmed@gmail.com>
2017-12-09 15:51:34 -0500

    add button to redirect to Map Activity

--- app/src/main/java/com/example/lewis/helloworld/MainActivity.java
+++ app/src/main/java/com/example/lewis/helloworld/MainActivity.java
@@ -9,24 +9,70 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.util.Log;
+import android.app.Dialog;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(isServicesOK()){
+            init();
+        }
     }
 
-    public void viewMapActivity(View v) {
-        Intent MapActivity = new Intent(this, MapsActivity.class);
-        startActivity(MapActivity);
+//    public void viewMapActivity(View v) {
+//        Intent MapActivity = new Intent(this, MapsActivity.class);
+//        startActivity(MapActivity);
+//    }
+
+    private void init(){
+        Button btnMap = (Button) findViewById(R.id.ToFindDistance);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, findDistance.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
 
 }
